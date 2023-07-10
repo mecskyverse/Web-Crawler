12f8a2a8ec37ee550e1eab41a35844f327478bf7
Aakash <vermaaakash1000@gmail.com>
2023-07-10 10:22:31 +0530

    Report printing functionality added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function main() {
+const {crawlPage} = require('./crawl')
+const {printReport} = require('./report.js')
+async function main() {
     const argument = process.argv.slice(2);
     if(argument.length === 1)
     console.log("WE have got the message")
@@ -6,7 +8,8 @@ function main() {
     throw new Error("Argument URL is more than 1")
     if(argument.length < 1 )
     throw new Error("Argument URL is less than 1")
-
-
+    const baseUrl = argument[0];
+    const pages = await crawlPage(baseUrl, baseUrl, {})
+    printReport(pages)
 }
 main()
