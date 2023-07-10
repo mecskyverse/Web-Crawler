3bca9670ed09968dc919dfdb04ccb24abca93db9
Aakash <vermaaakash1000@gmail.com>
2023-07-10 10:21:14 +0530

    Get URL from html and Crawl function added

--- crawl.js
+++ crawl.js
@@ -12,41 +12,85 @@ if(newUrl.length > 0 &&  newUrl.charAt(newUrl.length -1) === '/')
 {
     newUrl = newUrl.slice(0, -1)
 }
-console.log('newURL' , newUrl)
+// console.log('newURL' , newUrl)
 
 
 return newUrl
 }
 
 
-function getUrlFromHtml(htmlString){
-    try {
+function getUrlFromHtml(htmlString, baseUrl){
+    const urls = []
     const dom = new JSDOM(htmlString) 
     const document = dom.window.document
     const links = document.querySelectorAll('a')
-    const urls = Array.from(links).map(link => link.href)
-    const normalizeURLArray = urls.map(url => {}) 
-    return normalizeURLArray
+    for(const link of links){
+        if(link.href.slice(0,1) === '/'){
+            //relative URL
+            try {
+                const urlObj = new URL(`${baseUrl}${link.href}`)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log("Getting error from relative url",error.message)
+            }
+        }
+        else{
+            //Absolute URL
+            try {
+                const urlObj = new URL(link.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log("Getting an error in absolute url", error.message)
+            }
+        }
+    }
+    return urls
+}
+
+
+async function crawlPage(baseUrl, currentUrl, pages){
+  const currUrlObj = new URL(currentUrl)
+  const baseUrlObj = new URL(baseUrl)
+
+  if(currUrlObj.hostname !== baseUrlObj.hostname){
+      return pages;
+    }
+
+    const normalizedCurrUrl = normalizeURL(currentUrl)
+    if(pages[normalizedCurrUrl] > 0){
+      pages[normalizedCurrUrl]++;
+      return pages; 
+    }
+
+    pages[normalizedCurrUrl] = 1;
+    console.log(`Crawling this website currently at ${currentUrl}`)
+
+    try {
+      const response = await fetch(currentUrl);
+        if(!response.ok){
+            throw new Error("Getting Problem to fetch the website")
+        }
+        const contentType = response.headers.get('content-type');
+        if(!contentType.startsWith('text/html'))
+            {
+                console.log('contentType', contentType)
+                throw new Error('Content in the web page is not appropriate')
+            }
+        const htmlBody = await response.text()
+        const nextUrls = getUrlFromHtml(htmlBody, baseUrl)
+        
+        for(const nextUrl of nextUrls){
+          pages = await crawlPage(baseUrl, nextUrl, pages)
+        }
     } catch (error) {
         console.log(error.message)
     }
-    // console.log(urls)
-    // console.log(normalizeURLArray)
+    return pages;
 }
 
-htmlString = `<html>
-<body>
-    <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
-    <a href="https://blog.booty.dev"><span>New one</span></a>
-    <a href="https://blog.booti.dev"><span>Go to Boot.dev</span></a>
-</body>
-<a href="https://blog.booty.dev"><span>Go to Boot.dev</span></a>
 
-</html>
-`
-getUrlFromHtml(htmlString)
-normalizeURL('https://boot.Dev/path/')
 module.exports = {
     normalizeURL,
-    getUrlFromHtml
+    getUrlFromHtml,
+    crawlPage
 }
